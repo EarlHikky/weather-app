202f8af12b43bbd86472256e76b16e13924fbdb8
Earl Hikky <earlhikky91@gmail.com>
2024-07-17 15:49:57 +0300

    drf added

--- weather-api/weather_project/weather/urls.py
+++ weather-api/weather_project/weather/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import get_weather, autocomplete_city, search_statistics
+from .views import get_weather, autocomplete_city, SearchHistoryAPIView
 
 urlpatterns = [
     path('', get_weather, name='get_weather'),
     path('autocomplete/', autocomplete_city, name='autocomplete_city'),
-    path('api/search_statistics/', search_statistics, name='search_statistics'),
+    path('api/v1/search-list/', SearchHistoryAPIView.as_view()),
 ]
--- weather-api/weather_project/weather/views.py
+++ weather-api/weather_project/weather/views.py
@@ -1,92 +1,59 @@
-from re import match
-from string import capwords, punctuation
 from urllib.parse import quote, unquote
 
-import requests
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+from rest_framework.generics import ListAPIView
 
 from .models import SearchHistory
+from .serializers import SearchHistorySerializer
+from .utils import save_search_history, fetch_weather_data, get_context, validate_city, httpx_get
 
 
-def get_weather(request):
+class SearchHistoryAPIView(ListAPIView):
+    queryset = SearchHistory.objects.all()
+    serializer_class = SearchHistorySerializer
+
+
+def render_weather_response(request: WSGIRequest, context: dict) -> HttpResponse:
+    return render(request, 'weather/index.html', context)
+
+
+def get_weather(request: WSGIRequest) -> HttpResponse:
     weather_data = None
     last_city = request.COOKIES.get('last_city', '')
 
     if request.method == 'POST':
-        city = request.POST.get('city')
+        request_city = request.POST.get('city')
 
-        if city.isdigit() or all(char in punctuation for char in city):
-            return render(request,
-                          'weather/index.html',
-                          {'error': 'Invalid city name.',
-                           'last_city': 'Enter a valid city name'}
-                          )
+        if not validate_city(request_city):
+            return render_weather_response(request, {'last_city': 'Enter a valid city name'})
 
-        weather_data = fetch_weather_data(city)
-        if not weather_data:
-            return render(request,
-                          'weather/index.html',
-                          {'weather_data': weather_data,
-                           'last_city': city}
-                          )
+        city, weather_data, status_code = fetch_weather_data(request_city)
 
-        match_city = match(r'^[\w\s-]*\b', city)
-        if match_city:
-            city = capwords(match_city.group(0), sep='-')
+        if status_code:
+            return render_weather_response(request, {'last_city': 'Try a few moments later'})
 
-        search_history, created = SearchHistory.objects.get_or_create(city=city)
-        search_history.search_count += 1
-        search_history.save()
+        if weather_data is None:
+            return render_weather_response(request, {'last_city': request_city})
 
-        response = render(request,
-                          'weather/index.html',
-                          {'weather_data': weather_data,
-                           'last_city': city}
-                          )
-        response.set_cookie('last_city', quote(city))
+        save_search_history(city)
+        context = get_context(weather_data, request_city, city)
+        response = render_weather_response(request, context)
+        response.set_cookie('last_city', quote(request_city))
         return response
 
-    return render(request,
-                  'weather/index.html',
-                  {'weather_data': weather_data,
-                   'last_city': unquote(last_city)}
-                  )
+    context = {'last_city': unquote(last_city)}
+    return render_weather_response(request, context)
 
 
-def fetch_weather_data(city):
-    geocode_url = f'https://nominatim.openstreetmap.org/search?q={city}&format=json&limit=1'
-    geocode_response = requests.get(geocode_url)
-    if geocode_response.status_code == 200 and geocode_response.json():
-        location = geocode_response.json()[0]
-        latitude = location['lat']
-        longitude = location['lon']
-
-        weather_url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true'
-        weather_response = requests.get(weather_url)
-        if weather_response.status_code == 200:
-            data = weather_response.json()
-            return {
-                'city': city,
-                'temperature': data['current_weather']['temperature'],
-                'wind_speed': data['current_weather']['windspeed'],
-                'description': data['current_weather']['weathercode']
-            }
-    return None
-
-
-def autocomplete_city(request):
+def autocomplete_city(request: WSGIRequest) -> JsonResponse:
     if 'term' in request.GET:
         query = request.GET.get('term')
         geocode_url = f'https://nominatim.openstreetmap.org/search?q={query}&format=json&limit=5'
-        geocode_response = requests.get(geocode_url)
+        geocode_response = httpx_get(geocode_url)
         if geocode_response.status_code == 200:
             results = geocode_response.json()
             cities = [result['display_name'] for result in results]
             return JsonResponse(cities, safe=False)
     return JsonResponse([], safe=False)
-
-
-def search_statistics(request):
-    stats = SearchHistory.objects.all().values('city', 'search_count')
-    return JsonResponse(list(stats), safe=False)
